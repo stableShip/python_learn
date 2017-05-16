1623024fd6311e049f7e2dff43f3329f7e334ee5
stableShip <296888680@qq.com>
2017-05-16 15:56:00 +0800

    删除 日期测试文件, 完善 文件测试

--- fileTest.py
+++ fileTest.py
@@ -1,10 +1,16 @@
-file = open("./fileTest.py")
+# coding=utf-8
 
-text = file.read()
+# 读取文件方式一
+a_file = open("./fileTest.py")
+text = a_file.read()
+# 显示调用close
+a_file.close()
+print ("文件类型: %s, 第一行内容: %s" % (type(text), text[0:15]))
 
-print(type(text))
+# 读取文件方式二
+with open('./fileTest.py', 'r') as f:
+    print("第一行内容: %s" % f.readline())
 
-text = file.read()
-
-print(file.readline())
-
+# 写文件
+with open('./test.txt', 'w') as f:
+    f.write('Hello, world!')
