56017d35df269c2a8365aa9182ceb804b496d3c0
stableShip <296888680@qq.com>
2017-05-16 15:46:57 +0800

    完善 类测试

--- classTest.py
+++ classTest.py
@@ -1,39 +1,64 @@
-# coding=utf-8
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 __author__ = 'JIE'
 
 
-class Test():
-    __privateVaule = "privateVaule"
+class Test:
+    # 构造函数
+    def __init__(self):
+        # 实例变量
+        self.name = "name"
 
-    publicValue = 0
+    # 类私有变量, 不可以在外部访问到
+    __private_value = "private_value"
 
-    # def __init__(self, privateValue, publiceValue):
-    #     self.publicValue = publiceValue
-    #     self.__privateVaule = privateValue
+    # 类公开变量, 可以在外部访问到
+    public_value = "public_value"
 
+    # 实例私有方法(函数)
     def __test(self):
         print("private function")
 
-    def public(self):
+    # 实例
+    def public_func(self):
         print("public function")
 
-    def getPrivateValue(self):
-        return self.__privateVaule;
+    # 私有变量通过方法进行访问
+    def get_private_value(self):
+        return self.__private_value
 
+    # 静态方法
     @staticmethod
-    def staticTest():
-        print "static"
+    def static_test():
+        return "static"
 
+    # 类方法
     @classmethod
-    def classDefTest(self):
-        print("classDefTest")
+    def class_def_test(cls):
+        return "classDefTest"
 
-# print Test().publicValue
+# 不可访问私有变量
+try:
+    Test.__private_value
+except Exception, e:
+    print "类私有变量应该不能打印, 报错: ", e
 
-# print Test().getPrivateValue()
+# 可访问公开变量
+print "类公开变量:", Test.public_value
 
-# print Test(1, 2).getPrivateValue()
+# 实例(对象)可以调用类方法, 类变量, 静态方法
+print "实例调用类方法, 类变量, 静态方法: ", Test().public_value, Test().class_def_test(), Test().static_test()
 
-Test.staticTest()
+try:
+    # 类不能调用实例变量
+    Test.name
+except Exception, e:
+    print "类不能调用实例变量: ", e
 
-Test.classDefTest()
+try:
+    # 类不能调用实例方法
+    Test.public_func()
+except Exception, e:
+    print "类不能调用实例方法: ", e
+
+print "类能调用静态方法", Test.static_test()
