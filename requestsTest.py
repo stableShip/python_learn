df8df0b380f36ad286786b519b2d22cd47b7f677
stableShip <296888680@qq.com>
2017-05-17 16:02:00 +0800

    提交 项目依赖 提交 selenium测试

--- requestsTest.py
+++ requestsTest.py
@@ -11,3 +11,15 @@ sys.setdefaultencoding('utf-8')
 url = "http://www.baidu.com"
 data = requests.get(url)
 print "请求返回状态码: %s, 返回内容: %s" % (data.status_code, data.content)
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+browser = webdriver.Chrome()
+
+browser.get('http://www.baidu.com')
+assert "百度" in browser.title
+print browser.title
+
+
+browser.quit()
